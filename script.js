06af3a4e9b36233ead1d72cc2074e47b17a9984d
Tosin Akerele <akerele.james1699@gmail.com>
2023-02-21 19:55:14 +0100

    newest commits

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ year = document.getElementById("cal");
 let date = new Date();
 let calYear = date.getUTCFullYear();
 
-console.log(calYear);
-
 year.innerHTML = calYear;
-
 
 const hamburger = document.getElementById("open");
 const cancel = document.getElementById("close");
 const list = document.getElementById("linksmobile");
 
-
 hamburger.addEventListener("click", mobileMenu);
 cancel.addEventListener("click", closeMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    cancel.classList.toggle("active");
-    list.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  cancel.classList.toggle("active");
+  list.classList.toggle("active");
 }
 function closeMenu() {
-    cancel.classList.remove("active");
-    hamburger.classList.remove("active");
-    list.classList.remove("active");
+  cancel.classList.remove("active");
+  hamburger.classList.remove("active");
+  list.classList.remove("active");
 }
